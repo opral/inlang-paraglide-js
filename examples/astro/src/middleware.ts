adfe77092ae1a63b5abb3d6a152560fdc3d9fd92
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-01-14 11:44:30 -0800

    add skip https://github.com/opral/paraglide-js/issues/564

--- examples/astro/src/middleware.ts
+++ examples/astro/src/middleware.ts
@@ -2,5 +2,10 @@ import { paraglideMiddleware } from "./paraglide/server";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
+	// Avoid consuming bodies for non-GET/HEAD requests
+	// https://github.com/opral/paraglide-js/issues/564
+	if (context.request.method !== "GET" && context.request.method !== "HEAD") {
+		return next(context.request);
+	}
 	return paraglideMiddleware(context.request, ({ request }) => next(request));
 });
